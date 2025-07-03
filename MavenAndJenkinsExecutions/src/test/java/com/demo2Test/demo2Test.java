98062cf26282254db6ff5c0ff176709970c09751
lenovo <lenovo@DESKTOP-O9L4K3A>
2025-07-03 16:03:53 +0530

    First Commit For Jenkins

---
+++ MavenAndJenkinsExecutions/src/test/java/com/demo2Test/demo2Test.java
@@ -0,0 +1,12 @@
+package com.demo2Test;
+
+import org.testng.annotations.Test;
+
+public class demo2Test {
+	@Test
+	public void demo2Test() {
+		System.out.println(System.getProperty("url"));
+		System.out.println(System.getProperty("browser"));
+	}
+
+}
---
+++ MavenAndJenkinsExecutions/src/test/java/com/demoTest/demoTest.java
@@ -0,0 +1,12 @@
+package com.demoTest;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+	@Test
+	public void demoTest() {
+		System.out.println(System.getProperty("url"));
+		System.out.println(System.getProperty("browser"));
+	}
+
+}
